11c58afd27ddd32d7467f31eeec497f86f39ccd1
Adam Board <adamboard2003@gmail.com>
2023-02-02 14:44:53 +0000

    Added onto the lexer the different switch cases for each regex operator - Updating before working on the switch case for 'literals'

--- HoneyScoop/Source/Searching/RegexImpl/RegexLexer.cs
+++ HoneyScoop/Source/Searching/RegexImpl/RegexLexer.cs
@@ -8,19 +8,20 @@ internal class RegexLexer {
 
 	internal enum UnaryOpType {
 		AlternateEmpty, // ?
-		AlternateLoop // *
+		AlternateLoop, // *
+		AlternateLoopOnce // +
 	}
 	
 	internal interface IToken {
 		internal record struct Literal(byte val) : IToken;
 		internal record struct BinaryOp(BinaryOpType type) : IToken;
-		internal record struct UnaryOp();
-		internal record struct OpenParen();
-		internal record struct CloseParen();
+		internal record struct UnaryOp(UnaryOpType type) : IToken;
+		internal record struct OpenParen() : IToken;
+		internal record struct CloseParen() : IToken;
 	}
 
 	/// <summary>
-	/// Operators in input: <c>(), +, ?, *, |</c>
+	/// Operators in input: <c>(,), +, ?, *, |</c>
 	/// </summary>
 	/// <param name="src">The regex string</param>
 	/// <returns>A list of tokens that represents the regex string</returns>
@@ -30,6 +31,22 @@ internal class RegexLexer {
 		for (int i = 0; i < src.Length; i++) {
 			switch (src[i]) {
 				case '|':
+					tokens.Add(new IToken.BinaryOp(BinaryOpType.Alternate));
+					break;
+				case '(':
+					tokens.Add(new IToken.OpenParen());
+					break;
+				case ')':
+					tokens.Add(new IToken.CloseParen());
+					break;
+				case '+':
+					tokens.Add(new IToken.UnaryOp(UnaryOpType.AlternateLoopOnce));
+					break;
+				case '*':
+					tokens.Add(new IToken.UnaryOp(UnaryOpType.AlternateLoop));
+					break;
+				case '?':
+					tokens.Add(new IToken.UnaryOp(UnaryOpType.AlternateEmpty));
 					break;
 			}
 		}

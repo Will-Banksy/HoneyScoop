66c1cf0bb6e28d8c689ddd6be31f6b0cbfeae71b
Que-sar <100227246+Que-sar@users.noreply.github.com>
2023-02-06 18:26:06 +0000

    Viktor First Commit

--- HoneyScoop/Source/HoneyScoop.cs
+++ HoneyScoop/Source/HoneyScoop.cs
@@ -4,6 +4,8 @@ namespace HoneyScoop;
 /// This class is the main driver of the CLI application - It takes various options (although not parsing the CLI args itself)
 ///     and informed by them, uses various parts of the codebase to produce the appropriate output
 /// </summary>
+
+// I am Viktor
 internal class HoneyScoop {
 	internal HoneyScoop() {
 	}

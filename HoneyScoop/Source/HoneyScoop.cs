b56e5cd110dea66ea86c5dbcabfc52869baf5906
William Banks <williamjb555@gmail.com>
2023-02-24 00:53:31 +0000

    Removed some cringe I forgot to remove before

--- HoneyScoop/Source/HoneyScoop.cs
+++ HoneyScoop/Source/HoneyScoop.cs
@@ -4,13 +4,6 @@ namespace HoneyScoop;
 /// This class is the main driver of the CLI application - It takes various options (although not parsing the CLI args itself)
 ///     and informed by them, uses various parts of the codebase to produce the appropriate output
 /// </summary>
-
-// Please help, Will is overworking us, we need medical attention
-
-// NEVER GONNA GIVE YOU UP  NEVER GUNNNA LET YOU DOWN
-
-
-
 internal class HoneyScoop {
 	internal HoneyScoop() {
 	}

f53e75216a78c19358247502b515ca169eacc17d
Que-sar <100227246+Que-sar@users.noreply.github.com>
2023-03-10 14:49:16 +0000

    Update name to descriptive and new input argument
    Updated name to more descriptive and made new input argument

--- HoneyScoop/Source/CommandLineArguments.cs
+++ HoneyScoop/Source/CommandLineArguments.cs
@@ -2,18 +2,21 @@ using CommandLine;
 
 namespace HoneyScoop;
 
-internal class Helpers // TODO: Maybe rename this file/class to something more descriptive
+internal class CommandLineArguments 
 {
 	/// List of supported filetypes
 	internal readonly List<string> SupportedFormats = new List<string> { "jpg", "png", "gif", "mp4", "mp3", "wav", "xlsx", "pdf", "docx", "pptx", "zip" };
 
 	/// The commandlineparser library does not take a list of strings in
 	/// Converting it to a string makes handling them easier
-	public Helpers() {
+	public CommandLineArguments() {
 		FileTypes = string.Join(",", SupportedFormats);
 	}
 
 	/// Accepted arguments
+
+	[Option('i', "input_file", Required = true, HelpText = "The input file to conduct file reconstruction on.")]
+	public string InputFile { get; set; }
 
 	[Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
 	public bool Verbose { get; set; }
@@ -39,8 +42,8 @@ internal class Helpers // TODO: Maybe rename this file/class to something more d
 
 	internal List<string> ParseArgs(string[] arguments) {
 		List<string> definedFileTypes = new List<string>();
-		Parser.Default.ParseArguments<Helpers>(arguments)
-			.WithParsed<Helpers>(o => {
+		Parser.Default.ParseArguments<CommandLineArguments>(arguments)
+			.WithParsed<CommandLineArguments>(o => {
 					if(!string.IsNullOrEmpty(o.OutputDirectory)) {
 						OutputDirectory = o.OutputDirectory;
 					}
@@ -71,6 +74,15 @@ internal class Helpers // TODO: Maybe rename this file/class to something more d
 						Console.WriteLine("[+] The timestamps will be displayed.");
 					}
 
+					if (!File.Exists(o.InputFile))
+					{
+						Console.WriteLine("[-] The file supplied in does not exist. Please supply the path to the file in.");
+						System.Environment.Exit(0);
+					}
+
+					InputFile = o.InputFile;
+					Console.WriteLine($"[+] The reconstruction will take place on the following file: {o.InputFile}" );
+					
 					// String formatting magic because the commandLineParser does not like Lists
 
 					var fileTypes = o.FileTypes.Split(',');

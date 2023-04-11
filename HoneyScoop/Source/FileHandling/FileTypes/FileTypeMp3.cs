86fdd56c49449ebc4c28fffc8db2585f7399cccd
Que-sar <arato.viktor@gmail.com>
2023-04-11 19:43:33 +0100

    MP3, tagsize removed as it is not necessarily there, Frame Sync implemented and tested with MP3 files.

--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp3.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp3.cs
@@ -1,41 +1,46 @@
+using System.Text;
+using System.Text.RegularExpressions;
 namespace HoneyScoop.FileHandling.FileTypes;
+
 
 internal class FileTypeMp3 : IFileType {
 	public string Header => @"\x49\x44\x33";
 	public string Footer => ""; // Does not have a footer
 
-	private const int TagIDSize = 3; // Part of header
+	private const int TagIdSize = 3; // Part of header
 	private const int MajorVersionNumberSize = 1; // Part of header
 	private const int MinorVersionNumberSize = 1; // Usually just reserved
 	private const int TagUsageSize = 1; // Usually just reserved
 	private const int TagSizeSize = 4; // Size of the whole Tag
-
+	
+	/// <summary>
+	/// Checks if proper header length is present and converts the datastream into stream,
+	/// this then is checked with regex for an incomplete hex stream, which is 0000FFF or 0000FFE
+	/// representing the Synchronisation Frame of an MP3 file.
+	/// </summary>
+	/// <param name="data">The stream of data bytes that get checked.</param>
+	/// <returns>Returns whether the conditions of an mp3 file are present or not.</returns>
 	public AnalysisResult Analyse(ReadOnlySpan<byte> data) {
 		// Check if data is longer than header and is present
 		if(data.Length < (
-			   TagIDSize +
+			   TagIdSize +
 			   MajorVersionNumberSize +
 			   MinorVersionNumberSize +
 			   TagUsageSize +
 			   TagSizeSize)) {
-			return AnalysisResult.Unrecognised;
-		}
-
-		ReadOnlySpan<byte> tagData = data.Slice(0, 10);
-		ReadOnlySpan<byte> tagSizeBytes = tagData.Slice(6, 4);
-		int tagSize = BitConverter.ToInt32(tagSizeBytes);
-
-		// Check if the size of the data is not zero
-		if(tagSize == 0) {
 			return AnalysisResult.Corrupted;
 		}
 
-		// Check if the size of the data being examined is the same as
-		// the file defined in the header
-		if(data.Length != tagSize) {
-			return AnalysisResult.Corrupted;
+		// Converts data stream into string but still in hex form
+		string dataString = BitConverter.ToString(data.ToArray()).Replace("-", "\\x");
+		
+		// pattern matching for \x00\x00\xFF\xF|E followed by any possible hex char
+		string pattern = @"\\x00\\x00\\xFF\\x[FE][A-F0-9]"; 
+		if (Regex.IsMatch(dataString, pattern))
+		{
+			return AnalysisResult.Correct;
 		}
 
-		return AnalysisResult.Correct;
+		return AnalysisResult.Unrecognised;
 	}
 }

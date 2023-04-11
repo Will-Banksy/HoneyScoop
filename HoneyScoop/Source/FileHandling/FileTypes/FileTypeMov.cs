62db1c0ef140dc3dee193c2808b010ce8a8ab56c
Que-sar <arato.viktor@gmail.com>
2023-04-11 17:57:45 +0100

    .MOV and .MP4 all tested and modified to work properly.

--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeMov.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeMov.cs
@@ -3,24 +3,24 @@ using HoneyScoop.Util;
 
 namespace HoneyScoop.FileHandling.FileTypes {
 	internal class FileTypeMov : IFileType {
-		public string Header => @"\x00\x00\x00\x14\x66\x74\x79\x70";
+		public string Header => @"\x00\x00\x00\x20\x66\x74\x79\x70";
 		public string Footer => ""; // Does not have a footer
 		
 		private const int BrandSize = 4;
 		
 		private static readonly byte[][] SupportedBrands = {
 			new byte[] {0x71, 0x74, 0x20, 0x20}, // QuickTime Movie File
-			new byte[] {0x6D, 0x70, 0x34, 0x31}, // MPEG-4 file format version 1
-			new byte[] {0x6D, 0x70, 0x34, 0x32}, // MPEG-4 file format version 2
-			new byte[] {0x69, 0x73, 0x6F, 0x6D}, // ISO base media file format
-			new byte[] {0x6D, 0x34, 0x61, 0x20}, // MPEG-4 audio file format
-			new byte[] {0x6D, 0x34, 0x76, 0x20}, // MPEG-4 video file format
 			new byte[] {0x61, 0x76, 0x63, 0x31}, // Advanced Video Coding (AVC) file format
 			new byte[] {0x68, 0x65, 0x69, 0x63}, // High Efficiency Image Format (HEIF)
 			new byte[] {0x68, 0x65, 0x69, 0x78}, // High Efficiency Image Format (HEIF) (Raw)
 		};
 
-
+		/// <summary>
+		/// Checks if the header is of proper length and the first bytes are followed by bytes
+		/// recognised as ones located in .movs. 
+		/// </summary>
+		/// <param name="data">The stream of data bytes that get checked.</param>
+		/// <returns>Returns whether the conditions of a mov file are present or not.</returns>
 		public AnalysisResult Analyse(ReadOnlySpan<byte> data) {
 			// Check if data is longer than header and is present
 			if(data.Length < Header.Length) {
@@ -31,13 +31,13 @@ namespace HoneyScoop.FileHandling.FileTypes {
 			ReadOnlySpan<byte> brandData = data.Slice(8, BrandSize);
 			for (int i = 0; i < SupportedBrands.Length; i++)
 			{
-				if (!brandData.SequenceEqual(SupportedBrands[i]))
+				if (brandData.SequenceEqual(SupportedBrands[i]))
 				{
-					return AnalysisResult.Unrecognised;
+					return AnalysisResult.Correct;
 				}
 			}
 
-			return AnalysisResult.Correct;
+			return AnalysisResult.Unrecognised;
 		}
 	}
 }
--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp4.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp4.cs
@@ -2,7 +2,7 @@ using System.Text;
 
 namespace HoneyScoop.FileHandling.FileTypes {
 	internal class FileTypeMp4 : IFileType {
-		public string Header => @"\x66\x74\x79\x70"; // "ftyp" in hexadecimal format
+		public string Header => @"\x20\x66\x74\x79\x70"; // "ftyp" in hexadecimal format
 		public string Footer => ""; // Does not have a footer
 
 		private const int HeaderSize = 12; // Size of the header
@@ -13,9 +13,16 @@ namespace HoneyScoop.FileHandling.FileTypes {
 			new byte[] { 0x69, 0x73, 0x6F, 0x6D },
 			new byte[] { 0x4D, 0x34, 0x41, 0x20 },
 			new byte[] { 0x69, 0x73, 0x6F, 0x32 },
-			new byte[] { 0x6D, 0x70, 0x34, 0x31 },
+			new byte[] { 0x6D, 0x70, 0x34, 0x32 },
 			new byte[] { 0x4D, 0x34, 0x56, 0x20 }
 		};
+		
+		/// <summary>
+		/// Checks the header sizes and the whether the proper 4 following bytes are (some) of
+		/// the ones usually located in an mp4.
+		/// </summary>
+		/// <param name="data">The stream of data bytes that get checked.</param>
+		/// <returns> Returns whether the conditions of an mp4 file are present or not.</returns>
 		public AnalysisResult Analyse(ReadOnlySpan<byte> data) {
 			// Check if data is longer than header and is present
 			if(data.Length < HeaderSize) {
@@ -26,25 +33,25 @@ namespace HoneyScoop.FileHandling.FileTypes {
 			ReadOnlySpan<byte> brandData = data.Slice(8, BrandSize);
 			for (int i = 0; i < SupportedBrands.Length; i++)
 			{
-				if (!brandData.SequenceEqual(SupportedBrands[i]))
+				if (brandData.SequenceEqual(SupportedBrands[i]))
 				{
-					return AnalysisResult.Unrecognised;
+					// Check if data is not empty
+					if(data.Length <= HeaderSize) {
+						return AnalysisResult.Corrupted;
+					}
+
+					// Check if the file size is greater than the header size
+					ReadOnlySpan<byte> sizeData = data.Slice(4, 4);
+					int tagSize = BitConverter.ToInt32(sizeData);
+					if(tagSize <= HeaderSize) {
+						return AnalysisResult.Corrupted;
+					}
+
+					return AnalysisResult.Correct;
 				}
 			}
 
-			// Check if data is not empty
-			if(data.Length <= HeaderSize) {
-				return AnalysisResult.Corrupted;
-			}
-
-			// Check if the file size is greater than the header size
-			ReadOnlySpan<byte> sizeData = data.Slice(4, 4);
-			int tagSize = BitConverter.ToInt32(sizeData);
-			if(tagSize <= HeaderSize) {
-				return AnalysisResult.Corrupted;
-			}
-
-			return AnalysisResult.Correct;
+			return AnalysisResult.Unrecognised;
 		}
 	}
 }

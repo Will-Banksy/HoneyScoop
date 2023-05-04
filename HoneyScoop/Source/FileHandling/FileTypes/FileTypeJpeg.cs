cbaf3be70502060264a9aff0a622ac38d4a93e64
EmmBeeX <2003057@uad.ac.uk>
2023-05-04 22:32:24 +0100

    Added a AnalysisReuslt to the Jpeg aswell as made the header signature more specific towrds jpegs to decrease false psotives.

--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeJpeg.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeJpeg.cs
@@ -1,123 +1,177 @@
 namespace HoneyScoop.FileHandling.FileTypes;
 
 internal class FileTypeJpg : IFileType {
-	public string Header => @"\xFF\xD8"; // JPG signature
-	public string Footer => @"\xFF\xD9"; // End of Image (EOI) marker
-	public bool HasFooter => true;
-	public string FileExtension => "jpg";
+    public string Header => @"\xFF\xD8\xFF\xE0\x00\x10"; // JPG signature
+    public string Footer => @"\xFF\xD9"; // End of Image (EOI) marker
+    public bool HasFooter => true;
+    public string FileExtension => "jpg";
+    
+    public AnalysisResult Analyse(ReadOnlySpan<byte> data)
+    {
+        var segments = new List<Segment>();
+        int pos = HeaderSize;
 
-	private const int HeaderSize = 2;
-	private const int FooterSize = 2;
+        while (pos < data.Length - FooterSize)
+        {
+            var segment = Segment.Parse(data, pos);
+            if (segment == null)
+            {
+                break;
+            }
 
-	private readonly struct Segment {
-		internal const byte StartByte = 0xFF;
-		internal const byte EndByte = 0xD9;
+            segments.Add(segment.Value);
+            pos += segment.Value.TotalLength;
+        }
 
-		internal readonly byte Marker;
+        bool isValid = segments.Any() && segments.All(s => s.CheckDataValid() == AnalysisResult.Correct);
 
-		internal readonly int Length;
+        
+        bool headerMatched = false;
+        foreach (string header in Headers)
+        {
+            if (!data.Slice(0, header.Length).SequenceEqual(Encoding.ASCII.GetBytes(header))) continue;
+            headerMatched = true;
+            break;
+        }
 
-		// internal readonly ReadOnlySpan<byte> Data;
-		internal readonly bool IsValid;
-		internal readonly int TotalLength;
+        if (headerMatched)
+        {
+            return isValid ? AnalysisResult.Correct : AnalysisResult.Corrupted;
+        }
+        else
+        {
+            return AnalysisResult.Unrecognised;
+        }
+    }
+    
+    private const int HeaderSize = 6;
+    private const int FooterSize = 2;
 
-		/// <summary>
-		/// Construct a Segment, giving it a reference to the segment data, length, marker, and whether it is valid
-		/// </summary>
-		/// <param name="data"></param>
-		/// <param name="length"></param>
-		/// <param name="marker"></param>
-		/// <param name="isValid"></param>
-		private Segment(ReadOnlySpan<byte> data, int length, byte marker, bool isValid) {
-			Marker = marker;
-			Length = length;
-			// Data = data;
-			IsValid = isValid;
-			TotalLength = length + 2;
-		}
+    private readonly struct Segment {
+        internal const byte StartByte = 0xFF;
+        internal const byte EndByte = 0xD9;
 
-		/// <summary>
-		/// Checks whether the segment data matches what is expected of the segment type.
-		/// </summary>
-		/// <returns>An <see cref="AnalysisResult"/> indicating how the segment data matches it's expectations</returns>
-		internal AnalysisResult CheckDataValid() {
-			if(!IsValid) {
-				return AnalysisResult.Corrupted;
-			}
+        internal readonly byte Marker;
 
-			switch(Marker) {
-				case 0xC0:
-				case 0xC1:
-				case 0xC2:
-				case 0xC3:
-				case 0xC5:
-				case 0xC6:
-				case 0xC7:
-				case 0xC9:
-				case 0xCA:
-				case 0xCB:
-				case 0xCD:
-				case 0xCE:
-				case 0xCF:
-					return AnalysisResult.Correct;
+        internal readonly int Length;
 
-				default:
-					return AnalysisResult.FormatError;
-			}
-		}
+        
+        internal readonly bool IsValid;
+        internal readonly int TotalLength;
 
-		/// <summary>
-		/// Parse a segment from the given data, starting at the given position
-		/// </summary>
-		/// <param name="data"></param>
-		/// <param name="pos"></param>
-		/// <returns>A new Segment or null if the data is corrupted</returns>
-		internal static Segment? Parse(ReadOnlySpan<byte> data, int pos) {
-			if(data[pos] != StartByte) {
-				return null;
-			}
+      
+        /// <param name="data"></param>
+        /// <param name="length"></param>
+        /// <param name="marker"></param>
+        /// <param name="isValid"></param>
+        private Segment(ReadOnlySpan<byte> data, int length, byte marker, bool isValid) {
+            Marker = marker;
+            Length = length;
+            IsValid = isValid;
+            TotalLength = length + 2;
+        }
+        
 
-			int marker = data[pos + 1];
-			if(marker == EndByte) {
-				// End of Image (EOI) marker
-				return null;
-			}
+        /// <summary>
+        /// Checks whether the segment data matches what is expected of the segment type.
+        /// </summary>
+        /// <returns>An <see cref="AnalysisResult"/> indicating how the segment data matches it's expectations</returns>
+        internal AnalysisResult CheckDataValid() {
+            if(!IsValid) {
+                return AnalysisResult.Corrupted;
+            }
 
-			if(marker == 0) {
-				// Some editors use 0 as padding before the actual marker
-				return null;
-			}
+            switch(Marker) {
+                case 0xC0:
+                case 0xC1:
+                case 0xC2:
+                case 0xC3:
+                case 0xC5:
+                case 0xC6:
+                case 0xC7:
+                case 0xC9:
+                case 0xCA:
+                case 0xCB:
+                case 0xCD:
+                case 0xCE:
+                case 0xCF:
+                    return AnalysisResult.Correct;
 
-			int length = (data[pos + 2] << 8) + data[pos + 3];
-			if(length < 2 || length > data.Length - pos - 2) {
-				return null;
-			}
+                default:
+                    return AnalysisResult.FormatError;
+            }
+        }
 
-			ReadOnlySpan<byte> segmentData = data.Slice(pos + 4, length - 2);
-			bool isValid = data[pos + length - 1] == EndByte;
 
-			return new Segment(segmentData, length, (byte)marker, isValid);
-		}
-	}
+        /// <param name="data"></param>
+        /// <param name="pos"></param>
 
-	// TODO: finish this part
-	public AnalysisResult Analyse(ReadOnlySpan<byte> data) {
-		var segments = new List<Segment>();
-		int pos = HeaderSize;
+        internal static Segment? Parse(ReadOnlySpan<byte> data, int pos)
+        {
+            if (data[pos] != StartByte)
+            {
+                return null;
+            }
 
-		while(pos < data.Length - FooterSize) {
-			var segment = Segment.Parse(data, pos);
-			if(segment == null) {
-				break;
-			}
+            int marker = data[pos + 1];
+            if (marker == EndByte)
+            {
+                // End of Image (EOI) marker
+                return null;
+            }
 
-			segments.Add(segment.Value);
-			pos += segment.Value.TotalLength;
-		}
+            if (marker == 0)
+            {
+                // Some editors use 0 as padding before the actual marker
+                return null;
+            }
 
-		bool isValid = segments.Any() && segments.All(s => s.CheckDataValid() == AnalysisResult.Correct);
+            int length = (data[pos + 2] << 8) + data[pos + 3];
+            if (length < 2 || length > data.Length - pos - 2)
+            {
+                return null;
+            }
 
-		throw new NotImplementedException(); // Need to implement the FileInformation struct. Or just return an AnalysisResult which is what this method should return
-		// return new FileInformation(FileType.Jpg, isValid, segments.Count, data.Length, HeaderSize, FooterSize);
-	}
-}
+            ReadOnlySpan<byte> segmentData = data.Slice(pos + 4, length - 2);
+            bool isValid = data[pos + length - 1] == EndByte;
+
+            return new Segment(segmentData, length, (byte)marker, isValid);
+
+
+            AnalysisResult Analyse(ReadOnlySpan<byte> data)
+            {
+                var segments = new List<Segment>();
+                int pos = HeaderSize;
+
+                while (pos < data.Length - FooterSize)
+                {
+                    var segment = Segment.Parse(data, pos);
+                    if (segment == null)
+                    {
+                        break;
+                    }
+
+                    segments.Add(segment.Value);
+                    pos += segment.Value.TotalLength;
+                }
+
+                bool isValid = segments.Any() && segments.All(s => s.CheckDataValid() == AnalysisResult.Correct);
+
+                bool headerMatched = false;
+                foreach (string header in Headers)
+                {
+                    if (!data.Slice(0, header.Length).SequenceEqual(Encoding.ASCII.GetBytes(header))) continue;
+                    headerMatched = true;
+                    break;
+                }
+
+                if (headerMatched)
+                {
+                    return isValid ? AnalysisResult.Correct : AnalysisResult.Corrupted;
+                }
+                else
+                {
+                    return AnalysisResult.Unrecognised;
+                }
+            }
+        }

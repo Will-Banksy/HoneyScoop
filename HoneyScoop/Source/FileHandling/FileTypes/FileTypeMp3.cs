cb86d8fb1baee1e00611ca2d1c099aeaec56b31c
Que-sar <100227246+Que-sar@users.noreply.github.com>
2023-05-04 17:49:26 +0100

    Comment changing
    MP3 comment changed to using bitwise instead of regex.

--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp3.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeMp3.cs
@@ -17,7 +17,7 @@ internal class FileTypeMp3 : IFileType {
 	
 	/// <summary>
 	/// Checks if proper header length is present and converts the datastream into stream,
-	/// this then is checked with regex for an incomplete hex stream, which is 0000FFF or 0000FFE
+	/// this then is checked with bitwise operations for an incomplete hex stream, which is FFF or FFE (or FF0F, FF0E using >> 4)
 	/// representing the Synchronisation Frame of an MP3 file.
 	/// </summary>
 	/// <param name="data">The stream of data bytes that get checked.</param>
@@ -33,8 +33,7 @@ internal class FileTypeMp3 : IFileType {
 			return AnalysisResult.Corrupted;
 		}
 
-		// Converts data stream into string but still in hex form
-		// string dataString = BitConverter.ToString(data.ToArray()).Replace("-", "\\x");
+		// Bitwise checking for FFF or FFE Synchronisation frame.
 		for (int i = 0; i < data.Length - 1; i++)
 		{
 			if (data[i] == 0xFF && (data[i+1] >> 4) == 0x0F || (data[i+1] >> 4) == 0x0E)

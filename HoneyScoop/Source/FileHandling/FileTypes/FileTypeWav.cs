ddd7d516495f4f15a38831a0c40c955adf56ad49
Jed/Jam <jedbarron99@gmail.com>
2023-05-06 18:03:04 +0100

    Added Validations

--- HoneyScoop/Source/FileHandling/FileTypes/FileTypeWav.cs
+++ HoneyScoop/Source/FileHandling/FileTypes/FileTypeWav.cs
@@ -36,19 +36,19 @@ namespace HoneyScoop.FileHandling.FileTypes
             }
 
             // Read bytes 16 to 19 and convert them to an integer to get the Format Chunk Size
-            int formatChunkSize = BitConverter.ToInt32(data.Slice(16, 4).ToArray(), 0);
+            int formatChunkSize = BitConverter.ToInt32(data.Slice(16, 4));
 
             // Read bytes 20 to 21 and convert them to an unsigned short to get the Audio Format
-            ushort audioFormat = BitConverter.ToUInt16(data.Slice(20, 2).ToArray(), 0);
+            ushort audioFormat = BitConverter.ToUInt16(data.Slice(20, 2));
 
             // Read bytes 22 to 23 and convert them to an unsigned short to get the Number of Channels
-            ushort numberOfChannels = BitConverter.ToUInt16(data.Slice(22, 2).ToArray(), 0);
+            ushort numberOfChannels = BitConverter.ToUInt16(data.Slice(22, 2));
 
             // Read bytes 24 to 27 and convert them to an integer to get the Sample Rate
-            int sampleRate = BitConverter.ToInt32(data.Slice(24, 4).ToArray(), 0);
+            int sampleRate = BitConverter.ToInt32(data.Slice(24, 4));
 
             // Read bytes 34 to 35 and convert them to an unsigned short to get the Bits per Sample
-            ushort bitsPerSample = BitConverter.ToUInt16(data.Slice(34, 2).ToArray(), 0);
+            ushort bitsPerSample = BitConverter.ToUInt16(data.Slice(34, 2));
 
             // Read bytes after the Format Chunk (36 + formatChunkSize) and convert them to an ASCII string to check the Data Chunk
             string dataChunk = Encoding.ASCII.GetString(data.Slice(36 + formatChunkSize, 4));
